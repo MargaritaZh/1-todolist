a24a7a4303a010b5867a7787c33c82abfc77c9e2
MargaritaZh <margaritazhitkevich@gmail.com>
2024-07-26 23:16:57 +0300

    lesson-7 Material UI (add styles (sx={{...}}) for todolists)

--- src/Task.tsx
+++ src/Task.tsx
@@ -6,35 +6,43 @@ import Checkbox from '@mui/material/Checkbox';
 import ListItem from '@mui/material/ListItem';
 
 type Props = {
-    taskId:string
-    taskTitle:string
-    isDone:boolean
-    changeTaskStatusHandler:(e: ChangeEvent<HTMLInputElement>)=>void
-    upDateItemHandler:(newTitle: string)=>void
-    removeTaskHandler:()=>void
+    taskId: string
+    taskTitle: string
+    isDone: boolean
+    changeTaskStatusHandler: (e: ChangeEvent<HTMLInputElement>) => void
+    upDateItemHandler: (newTitle: string) => void
+    removeTaskHandler: () => void
 };
 export const Task = (props: Props) => {
     return (
-        <ListItem key={props.taskId}>
-            {/*<input*/}
-            {/*    type="checkbox"*/}
-            {/*    checked={props.isDone}*/}
-            {/*    onChange={props.changeTaskStatusHandler}*/}
-            {/*/>*/}
-            <Checkbox  checked={props.isDone}
-                       onChange={props.changeTaskStatusHandler} />
+        <ListItem key={props.taskId} sx={{
+            padding: "0px",
+            justifyContent: "space-between",
+            opacity: props.isDone ? 0.5 : 1,
+        }}>
 
-            {/*<span className={task.isDone ? "is-done" : "task"}>{task.title}</span>*/}
-            <EditableSpan
-                oldTitle={props.taskTitle}
-                isDone={props.isDone}
-                upDateItem={props.upDateItemHandler}/>
+            <div>
+                {/*<input*/}
+                {/*    type="checkbox"*/}
+                {/*    checked={props.isDone}*/}
+                {/*    onChange={props.changeTaskStatusHandler}*/}
+                {/*/>*/}
+                <Checkbox checked={props.isDone}
+                          onChange={props.changeTaskStatusHandler}/>
+
+                {/*<span className={task.isDone ? "is-done" : "task"}>{task.title}</span>*/}
+                <EditableSpan
+                    oldTitle={props.taskTitle}
+                    isDone={props.isDone}
+                    upDateItem={props.upDateItemHandler}/>
+
+            </div>
 
             {/*<Button title={"X"} onclickHandler={props.removeTaskHandler}/>*/}
             <IconButton aria-label="delete"
                         onClick={props.removeTaskHandler}
             >
-                <DeleteIcon fontSize="inherit" />
+                <DeleteIcon fontSize="inherit"/>
             </IconButton>
         </ListItem>
     );
